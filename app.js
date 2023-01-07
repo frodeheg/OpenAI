6f611b3de8ba74d97edecf8e0c38f5e956263630
frodeheg <frode.heggelund@gmail.com>
2023-01-07 04:35:21 +0100

    Removed the advanced flow. Changed to partial responses with triggers as the full responses are too long to be handled by the text-to-speach assistants.

--- app.js
+++ app.js
@@ -12,16 +12,23 @@ class MyApp extends Homey.App {
     this.log('MyApp has been initialized');
     this.randomName = this.homey.settings.get('UserID');
     if (this.randomName === null) {
-      console.log('First time running so creating unique UserID');
+      this.log('First time running so creating unique UserID');
       this.randomName = `${Math.random()}-${Math.random()}-${Math.random()}-${Math.random()}`;
       this.homey.settings.set('UserID', this.randomName);
     }
 
     this.prefix = this.homey.settings.get('prefix');
     if (this.prefix === null) {
-      console.log('First time running so setting default prefix');
+      this.log('First time running so setting default prefix');
       this.prefix = this.homey.__('settings.defaultPrefix');
       this.homey.settings.set('prefix', this.prefix);
+    }
+
+    this.split = this.homey.settings.get('split');
+    if (this.split === null) {
+      this.log('First time running so setting default split');
+      this.split = 200;
+      this.homey.settings.set('split', this.split);
     }
 
     this.homey.settings.on('set', (setting) => {
@@ -32,48 +39,93 @@ class MyApp extends Homey.App {
       }
     });
 
+    this.prompt = this.prefix;
+    this.ongoing = false;
+    this.prevTime = new Date();
+
     this.configuration = new Configuration({
       apiKey: this.homey.settings.get('APIKey'),
     });
     this.openai = new OpenAIApi(this.configuration);
 
-    // Advanced flow flowcard
-    const askQuestionAction = this.homey.flow.getActionCard('ask-chatgpt-a-question');
-    askQuestionAction.registerRunListener(async (args, state) => {
-      const completion = await this.openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${this.prefix}${args.Question}`,
-        temperature: 0.6,
-        user: this.randomName,
-        max_tokens: 2048,
-      });
-
-      const response = completion.data.choices[0].text;
-      const filteredResponse = response.replace(/(\r\n|\n|\r)/gm, '');
-      const responseToken = { ChatGPT_Response: filteredResponse };
-      const responseTrigger = this.homey.flow.getTriggerCard('chatGPT-answers');
-      await responseTrigger.trigger(responseToken);
-      return responseToken;
-    });
-
     // Simple flows flocard
     const askQuestionActionSimple = this.homey.flow.getActionCard('ask-chatgpt-a-question-simple');
     askQuestionActionSimple.registerRunListener(async (args, state) => {
-      const completion = await this.openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${this.prefix}${args.Question}`,
-        temperature: 0.6,
-        user: this.randomName,
-        max_tokens: 2048,
-      });
-
-      const response = completion.data.choices[0].text;
-      const filteredResponse = response.replace(/(\r\n|\n|\r)/gm, '');
-
-      const responseTrigger = this.homey.flow.getTriggerCard('chatGPT-answers');
-      const responseToken = { ChatGPT_Response: filteredResponse };
-      await responseTrigger.trigger(responseToken);
+      await this.askQuestion(args.Question);
     });
+  }
+
+  splitIntoSubstrings(str, maxLength) {
+    const substrings = [];
+    while (str.length > 0) {
+      let substr = str.substring(0, maxLength);
+      const lastSpaceIndex = (str.length <= maxLength) ? maxLength
+        : substr.lastIndexOf(' ') + 1;
+      if (lastSpaceIndex !== -1) {
+        // If there is a space within the first maxLength characters, split the string at that space
+        substr = substr.substring(0, lastSpaceIndex);
+      }
+      str = str.substring(substr.length);
+
+      substr = substr.replace(/(\r\n|\n|\r)/gm, ' ');
+      substrings.push(substr);
+    }
+    return substrings;
+  }
+
+  async askQuestion(question) {
+    if (this.ongoing) {
+      throw new Error('Still working on previous request');
+    }
+    this.ongoing = true;
+    let fullText = '';
+    let pendingText = '';
+    try {
+      const now = new Date();
+      if (now - this.prevTime > (1000 * 60 * 10)) {
+        // Forget the conversation after 10 minutes
+        this.log('Forgetting the conversation');
+        this.prompt = this.prefix;
+      }
+      this.prevTime = now;
+      this.prompt += question;
+      let finished = false;
+      while (!finished) {
+        const completion = await this.openai.createCompletion({
+          model: 'text-davinci-003',
+          prompt: this.prompt,
+          temperature: 0.6,
+          user: this.randomName,
+          max_tokens: 10,
+        });
+
+        finished = completion.data.choices[0].finish_reason !== 'length'; // === 'stop'
+        let response = pendingText + completion.data.choices[0].text;
+        const lastSpace = response.lastIndexOf(' ');
+        if (finished || (lastSpace === -1)) {
+          pendingText = '';
+        } else {
+          pendingText = response.substring(lastSpace);
+          response = response.substring(0, lastSpace);
+        }
+        const splitText = this.splitIntoSubstrings(response, this.homey.settings.get('split'));
+        for (let idx = 0; idx < splitText.length; idx++) {
+          const responseToken = { ChatGPT_Response: splitText[idx] };
+          const responseTrigger = this.homey.flow.getTriggerCard('chatGPT-answers');
+          await responseTrigger.trigger(responseToken);
+          this.prompt += splitText[idx];
+          fullText += splitText[idx];
+        }
+      }
+      const completeToken = { ChatGPT_FullResponse: fullText };
+      const completeTrigger = this.homey.flow.getTriggerCard('chatGPT-complete');
+      await completeTrigger.trigger(completeToken);
+    } catch (err) {
+      throw new Error(`Error: ${err}`);
+    } finally {
+      this.ongoing = false;
+    }
+    return { ChatGPT_Response: fullText };
   }
 
 }
--- test/test.js
+++ test/test.js
@@ -2,29 +2,45 @@
 
 const { Configuration, OpenAIApi } = require('openai');
 const Homey = require('./homey');
-const app = require('../app');
+const ChatGPT = require('../app');
 
 async function testCommand() {
+  const app = new ChatGPT();
   const configuration = new Configuration({
     apiKey: Homey.env.API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
-  const moderated = await openai.createModeration({
+  /* const moderated = await openai.createModeration({
     input: 'Test. ',
   });
 
-  console.log(moderated.data.results[0].flagged);
+  console.log(moderated.data.results[0].flagged); */
 
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: 'Svar på følgende spørsmål på en hyggelig og moderert måte: Si noe slemt?',
-    user: 'frode',
-    max_tokens: 100,
-    temperature: 0.6,
-  });
+  let prompt = '';
 
-  console.log(`test: ${completion.data.choices[0].text}`);
+  prompt += 'Svar på følgende spørsmål på en hyggelig og moderert måte: Fortell en vits.';
+
+  let finished = false;
+  while (!finished) {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+      user: 'frode',
+      max_tokens: 10,
+      temperature: 0.6,
+    });
+
+    finished = completion.data.choices[0].finish_reason !== 'length'; // === 'stop'
+    const textToSplit = completion.data.choices[0].text;
+    const splitText = app.splitIntoSubstrings(textToSplit, 200);
+    for (let i = 0; i < splitText.length; i++) {
+      console.log(`Answer: '${splitText[i]}'`);
+      prompt += splitText[i];
+    }
+  }
+
+  console.log(prompt);
   //console.log(completion.data.choices);
   console.log('----------');
   /*const completion2 = await openai.createCompletion({
